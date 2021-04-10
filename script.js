a946524840bdc93ac76227176e625f277d93b7be
unknown <rahulrajbhar19999@gmail.com>
2021-04-10 12:12:19 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,111 @@
+// //console.log("Hellow world")
+
+
+
+// var a = 17;
+// var b= 17;
+
+// // var final = newnum;
+// // console.log(a!=b)
+
+
+// // if(a==0){
+
+// //     console.log("truw")
+// // }
+// // else{
+
+// //     console.log("flase")
+// // }
+
+
+// // console.log((a>=18) ? "Yor are alegible": "Yor are not eligible");
+
+
+// // var final= 2;
+
+// // switch(final){
+// //     case 1: console.log("You enter 1");
+  
+// //     case 3: console.log("You enter 3")
+ 
+// //     case 2: console.log("You enter 2")
+   
+// //     case 4: console.log("You enter 4")
+  
+// //     case 5: console.log("You enter 5")
+// //     default:
+
+// // }
+
+
+// function sum(a,b){
+
+//     // var a=10; b=40;
+//     var total = a+b;
+// //    console.log(total);
+
+// }
+
+
+// function subtract(x,y){
+
+//     return  z = x+y;
+// }
+// // var sume=sum(5,9);
+// // sume;
+
+// //  console.log(subtract(4,9));
+
+
+// const arr1= [1,4,6,3,5];
+
+// let newArr = arr1.map((curElem,index,arr) ={
+//         return curElem>9;
+// })
+{/* <span id= "temp" class="spn-class"> </span> */}
+
+// var str1 = 'Rahul';
+// var str2 = 'Rajbhar';
+// var str3 = 'hello world';
+// var str4 = 'Java script programing';
+
+// // console.log(str4);
+// var temp=`${str1} RRRRRR ${str2} AAAAAA ${str2}`;
+// // console.log("demo "+str1+" demo2 "+str2+str3);
+// // console.log(temp);
+// var slicElement = str2.slice(2,5)
+// // console.log(slicElement)
+
+
+// var replaceFun = str3.replace('hello','rohan');
+
+// console.log(replaceFun);
+
+
+// Dailog Funcation in JavaScript
+
+// ------>  alert
+// ------>  prompt
+// ------>  confirm
+
+
+
+
+// alert("This is first alert box")
+
+// let name1 = prompt("Enter Your Right NAme","");
+
+// document.write(name1);
+// console.log(name1);
+
+// let conformation = confirm("Are you programer")
+// console.log(conformation);
+
+
+// if(conformation){
+//     console.log("Your chose OK");
+// }
+// else{
+//     console.log("You are clicked on cancel button");
+// }
